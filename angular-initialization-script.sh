c3a77130356a40c3234b62730d44169cbf8a7fba
pdmxdd <pdmxdd@gmail.com>
2022-05-05 11:38:37 -0500

    angular

---
+++ angular-initialization-script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Install Dependencies
+
+## Update Package Repositories
+sudo apt update -y
+
+## Install Git
+sudo apt install git
+
+## Install Caddy
+
+### Add Caddy Package
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+
+### Update Package Repositories
+
+sudo apt update -y
+
+### Install Caddy
+sudo apt install caddy
+
+# Download Project Artifacts
+git clone https://github.com/LaunchCodeTechnicalTraining/angular-tour-of-heroes-artifacts
+
+# Configure Web Server
+
+## Create Caddyfile
+(
+cat <<'EOF'
+https://localhost {
+    root * /home/student/angular-tour-of-heroes-artifacts/
+    file_server
+}
+EOF
+) > Caddyfile
+
+## Reload Caddy
+sudo caddy reload
